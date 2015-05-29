6c71ec15b13d6f2c0c20f55c08a9dd266ef3702d
Guillaume Roffé <guillaume.roffe@epita.fr>
2015-05-29 15:03:10 +0200

    Travellers added

---
+++ Assets/Scripts/Traveller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Traveller : MonoBehaviour {
+
+	public Node destination;
+	public Node current;
+	public bool transit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
