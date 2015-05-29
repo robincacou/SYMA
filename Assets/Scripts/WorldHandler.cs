a95dec71c2685eceabc3105d6d5a6a69da2d84dd
Robin Cassan <robin.cassan@epita.fr>
2015-05-29 16:07:04 +0200

    [WORLD] Add WorldHandler and transitions to each node

---
+++ Assets/Scripts/WorldHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldHandler : MonoBehaviour {
+
+	public GameObject NodesGO;
+	public GameObject TransitionsGO;
+
+	private Node[] nodes;
+	private Transition[] transitions;
+
+	void Start ()
+	{
+		nodes = NodesGO.GetComponentsInChildren<Node>();
+		transitions = TransitionsGO.GetComponentsInChildren<Transition>();
+
+		AssignTransitionsToNodes();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+			foreach(Node node in nodes)
+				node.DebugTrans();
+	}
+
+	private void AssignTransitionsToNodes()
+	{
+		foreach(Transition trans in transitions)
+		{
+			trans.first.AddTransition(trans);
+			trans.second.AddTransition(trans);
+		}
+	}
+}
