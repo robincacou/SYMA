f79287b638541a84cf51fbc5540c62e86864cbc7
Robin Cassan <robin.cassan@epita.fr>
2015-05-29 14:46:53 +0200

    [NETWORK] Made nodes and transitions

---
+++ Assets/Scripts/Network/Transition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transition : MonoBehaviour {
+
+	public Node first;
+	public Node second;
+
+	public uint weight;
+
+
+	private LineRenderer line;
+
+	void Start ()
+	{
+		line = GetComponent<LineRenderer>();
+
+		line.SetPosition(0, first.transform.position);
+		line.SetPosition(1, second.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
