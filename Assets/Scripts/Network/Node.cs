913ede151994c9bdcf57ed128b987880798834b3
Robin Cassan <robin.cassan@epita.fr>
2015-05-29 14:22:14 +0200

    [MISC] Correction on camera and scene

---
+++ Assets/Scripts/Network/Node.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	public TextMesh text;
+
+	void Start()
+	{
+		text.text = "Node: " + name;
+	}
+
+	void Update()
+	{
+	
+	}
+}
