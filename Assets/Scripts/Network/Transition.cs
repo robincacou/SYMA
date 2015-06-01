2e38d2bb80a43cd7bc862b9e5cdf03ab664b8e80
zakhab@hotmail.fr <zakhabri@gmail.fr>
2015-06-01 16:57:04 +0200

    Add clicking on transitions => slows them down (via alteredweight)

--- Assets/Scripts/Network/Transition.cs
+++ Assets/Scripts/Network/Transition.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Transition : MonoBehaviour {
-
+public class Transition : MonoBehaviour
+{
 	public TextMesh text;
 	public Node first;
 	public Node second;
@@ -11,21 +11,25 @@ public class Transition : MonoBehaviour {
 	public uint alteredWeight = 0;
 
 	private LineRenderer line;
+	private BoxCollider box;
 
-	void Start ()
+	void Start()
 	{
 		line = GetComponent<LineRenderer>();
+		box = GetComponentInChildren<BoxCollider>();
 
 		line.SetPosition(0, first.transform.position);
 		line.SetPosition(1, second.transform.position);
-
+		transform.position = Vector3.Lerp(first.transform.position, second.transform.position, 0.5f);
 		text.text = "" + initialWeight;
-		text.transform.position = Vector3.Lerp(first.transform.position, second.transform.position, 0.5f);
+		box.size = new Vector3(1.5f, 1, Vector3.Distance(first.transform.position, second.transform.position));
+		box.transform.LookAt(first.transform);
 	}
 
 	// TODO When weight changes, update the text
 
-	void Update () {
-	
+	void Update()
+	{
+
 	}
 }
