620ec50e6165a25f11594a30559bbc9c58fd155e
Robin Cassan <robin.cassan@epita.fr>
2015-06-04 01:10:36 +0200

    [GENERATOR] Basic transitions

--- Assets/Scripts/Network/Generator.cs
+++ Assets/Scripts/Network/Generator.cs
@@ -5,10 +5,15 @@ public class Generator : MonoBehaviour {
 
 	public Node NodePrefab;
 	public GameObject NodesContainer;
+	public Transition TransitionPrefab;
+	public GameObject TransitionContainer;
 
 	public uint unitsBetweenNodes;
+	public uint randomUnitsBetweenNodes;
 
-	void Start ()
+	private ArrayList Nodes;
+
+	void Awake ()
 	{
 		GenerateGraph(36);
 	}
@@ -19,12 +24,52 @@ public class Generator : MonoBehaviour {
 
 	public void GenerateGraph(uint numberOfNodes)
 	{
-		uint size = (uint)Mathf.Ceil(Mathf.Sqrt(numberOfNodes));
+		Nodes = new ArrayList();
+		int size = (int)Mathf.Ceil(Mathf.Sqrt(numberOfNodes));
 		for (uint i = 0; i < numberOfNodes; i++)
 		{
 			Node node = (Node)Instantiate(NodePrefab);
 			node.transform.parent = NodesContainer.transform;
-			node.transform.position = new Vector3(unitsBetweenNodes * (i % size), 0, unitsBetweenNodes * (i / size));
+			node.transform.position = new Vector3(unitsBetweenNodes * (i % size) + Random.Range(-randomUnitsBetweenNodes, randomUnitsBetweenNodes), 0,
+			                                      unitsBetweenNodes * (i / size) + Random.Range(-randomUnitsBetweenNodes, randomUnitsBetweenNodes));
+			Nodes.Add(node);
+		}
+
+		for (int i = 0; i < Nodes.Count; i++)
+		{
+			Node node = (Node)Nodes[i];
+			ArrayList possibleNeighbors = new ArrayList();
+
+			// TODO ADD DIAGONALS
+			// TODO DO NOT TAKE ALREADY CONNECTED NODES
+
+			// Top
+			if (i - size >= 0)
+				possibleNeighbors.Add(Nodes[i - size]);
+			// Right
+			if ((i % size) != (size - 1))
+				possibleNeighbors.Add(Nodes[i + 1]);
+			// Bottom
+			if (i + size < Nodes.Count)
+				possibleNeighbors.Add(Nodes[i + size]);
+			// Left
+			if ((i % size) != 0)
+				possibleNeighbors.Add(Nodes[i - 1]);
+
+			// Multiple random call and removing items are needed in order to pick random neighbors
+			int neighborNumber = Random.Range(1, possibleNeighbors.Count);
+			for (int j = 0; j < neighborNumber; j++)
+			{
+				int indexToConnect = Random.Range(0, possibleNeighbors.Count);
+				Node toConnect = (Node)possibleNeighbors[indexToConnect];
+				possibleNeighbors.RemoveAt(indexToConnect);
+
+				Transition trans = (Transition)Instantiate(TransitionPrefab);
+				trans.first = node;
+				trans.second = toConnect;
+				trans.initialWeight = (uint)Random.Range(10, 1000);
+				trans.transform.parent = TransitionContainer.transform;
+			}
 		}
 	}
 }
